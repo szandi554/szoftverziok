566933549438721690d53666201e9a6780ef11c9
S. Alexandra <szandi554@freemail.hu>
2025-11-21 20:26:20 +0100

    Folytatás

--- src/app/verziokezeles/verziokezeles.component.ts
+++ src/app/verziokezeles/verziokezeles.component.ts
@@ -1,3 +1,13 @@
+/*
+* File: verziokezeles.component.ts
+* Author: Siegenthaler Alexandra 
+* Copyright: 2025, Siegenthaler Alexandra 
+* Group: Szoft II/2/E
+* Date: 2025-11-21
+* Github: Siegenthaler Alexandra
+* Licenc: MIT
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
