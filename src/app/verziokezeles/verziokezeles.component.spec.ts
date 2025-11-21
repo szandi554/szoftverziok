4f02d1dcc1a521de0a4dc29e1048d029d37bd9b4
S. Alexandra <szandi554@freemail.hu>
2025-11-21 20:08:28 +0100

    Projekt létrehozás

---
+++ src/app/verziokezeles/verziokezeles.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerziokezelesComponent } from './verziokezeles.component';
+
+describe('VerziokezelesComponent', () => {
+  let component: VerziokezelesComponent;
+  let fixture: ComponentFixture<VerziokezelesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerziokezelesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VerziokezelesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
