cabfb3d260f9eaaf77a68161b755f210c85917fa
szandi554@freemail.hu <szandi554@freemail.hu>
2025-11-23 09:05:53 +0100

    Kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { VerziokezelesComponent } from './verziokezeles/VerziokezelesComponent';
+import { VerziokezelesComponent } from "./verziokezeles/verziokezeles.component";
 
 @Component({
   selector: 'app-root',
